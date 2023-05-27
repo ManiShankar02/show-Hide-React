821a4840f6cb64a5581f18ab2ed9d262b05e86de
Mani <125630807+ManiShankar02@users.noreply.github.com>
2023-05-27 12:10:31 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,44 @@
+import {Component} from 'react'
+
+import './index.css'
+
+class ShowHide extends Component {
+  state = {
+    showFirstName: false,
+    showLastName: false,
+  }
+
+  firstName = () => {
+    this.setState(prevState => ({showFirstName: !prevState.showFirstName}))
+  }
+
+  lastName = () => {
+    this.setState(prevState => ({showLastName: !prevState.showLastName}))
+  }
+
+  render() {
+    const {showFirstName, showLastName} = this.state
+
+    return (
+      <div className="container">
+        <h1 className="heading">Show/Hide</h1>
+        <div className="show-container">
+          <div className="name-container">
+            <button type="button" className="button" onClick={this.firstName}>
+              Show/Hide Firstname
+            </button>
+            {showFirstName && <p className="name">Joe</p>}
+          </div>
+          <div className="name-container">
+            <button type="button" className="button" onClick={this.lastName}>
+              Show/Hide Lastname
+            </button>
+            {showLastName && <p className="para">Jonas</p>}
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default ShowHide
